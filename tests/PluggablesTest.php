a05437f5f76496889259fa372646f4401d503720
Alleo Indong <freedom_war550@live.com.ph>
2015-06-13 01:10:37 +0800

    updated test

--- tests/PluggablesTest.php
+++ tests/PluggablesTest.php
@@ -8,20 +8,23 @@ class PluggablesTest extends PHPUnit_Framework_TestCase
 {
     protected $config;
     protected $files;
-    protected $module;
+    protected $pluggable;
+
     public function setUp()
     {
         parent::setUp();
         $this->config  = m::mock('Illuminate\Config\Repository');
         $this->files   = m::mock('Illuminate\Filesystem\Filesystem');
-        $this->module  = new Modules($this->config, $this->files);
+        $this->pluggable  = new Pluggables($this->config, $this->files);
     }
+
     public function tearDown()
     {
         m::close();
     }
+
     public function testHasCorrectInstance()
     {
-        $this->assertInstanceOf('Caffeinated\Modules\Modules', $this->module);
+        $this->assertInstanceOf('Aindong\Pluggables\Pluggables', $this->pluggable);
     }
 }
